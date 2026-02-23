803381e5b77964396f4179f7a585499fada9beaf
asahhiy <asahhiy@gmail.com>
2026-02-23 17:59:34 +0900

    motionを追加

--- src/components/AboutmeComponent.tsx
+++ src/components/AboutmeComponent.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import udonProfile from '../../public/udonProfile.png'
 import AboutMeText from './aboutme/AboutMeText'
-
+import { motion } from 'motion/react'
 
 
 export default function AboutmeComponent() {
