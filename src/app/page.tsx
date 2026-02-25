e254d4bdc14f045183d7e8299d5899003b106e23
asahhiy <asahhiy@gmail.com>
2026-02-25 16:36:23 +0900

    fix/とりまグリッドデザイン実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,38 @@ import WorksComponent from "@/components/WorksComponent"
 
 export default function page() {
   return (
-    <div className="items-center">
-      <p className='font-bebas text-9xl flex justify-center'>Portfolio</p>
-      <section id="about">
-        <AboutmeComponent />
-      </section>
-      <section id="skills">
-        <SkillsComponent />
-      </section>
-      <section id="works">
-        <WorksComponent />
-      </section>
-      <section id="contact">
-        <ContactComponent />
-      </section>
+    <div className="items-center space-y-4">
+      <div className="grid grid-cols-12 gap-4 mt-10 mx-10" >
+        <div className="col-span-12 lg:col-span-4 bg-amber-500 h-auto rounded-2xl">
+          <section id="contact">
+            <ContactComponent />
+          </section>
+
+        </div>
+        <div className="col-span-12 lg:col-span-5 bg-amber-400 h-auto rounded-2xl" />
+        <div className="col-span-12 lg:col-span-3 bg-amber-300 h-auto rounded-2xl" />
+        <div className="col-span-12 lg:col-span-2 bg-amber-300 h-auto rounded-2xl" />
+        <div className="col-span-12 lg:col-span-8 bg-amber-300 h-auto rounded-2xl" >
+          <p className='font-bebas text-9xl flex justify-center'>Portfolio</p>
+          <section id="about">
+            <AboutmeComponent />
+          </section>
+
+        </div>
+        <div className="col-span-12 lg:col-span-2 bg-amber-300 h-auto rounded-2xl" />
+        <div className="col-span-12 lg:col-span-4 bg-amber-300 h-auto rounded-2xl space-y-5">
+          <section id="works">
+            <WorksComponent />
+          </section>
+        </div>
+        <div className="col-span-12 lg:col-span-8 bg-amber-300 h-auto rounded-2xl" >
+          <section id="skills">
+            <SkillsComponent />
+          </section>
+        </div>
+      </div>
+
+
 
     </div>
   )
